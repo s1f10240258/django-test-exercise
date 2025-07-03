b51c4c762fd29771576c7491ada14433dba3e400
1F10240203 <s1f102402035@iniad.org>
2025-07-03 11:05:59 +0900

    fix3

--- todo/tests.py
+++ todo/tests.py
@@ -16,7 +16,6 @@ class TaskModelTestCase(TestCase):
         self.assertEqual(task.title, "task1")
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, due)
-        
 
     def test_create_task2(self):
         task = Task(title="task2")
@@ -26,7 +25,6 @@ class TaskModelTestCase(TestCase):
         self.assertEqual(task.title, "task2")
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, None)
-
 
     def test_is_overdue_future(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
@@ -44,9 +42,7 @@ class TaskModelTestCase(TestCase):
 
         self.assertTrue(task.is_overdue(current))
 
-
     def test_is_overdue_none(self):
-        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
         task = Task(title="task1")
         task.save()
@@ -64,7 +60,6 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, "todo/index.html")
         self.assertEqual(len(response.context["tasks"]), 0)
     
-
     def test_index_post(self):
         client = Client()
         data = {"title": "Test Task", "due_at": "2024-06-30 23:59:59"}
@@ -73,7 +68,6 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
         self.assertEqual(len(response.context["tasks"]), 1)
-
 
     def test_index_get_order_post(self):
         task1 = Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
@@ -87,7 +81,6 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, "todo/index.html")
         self.assertEqual(response.context["tasks"][0], task2)
         self.assertEqual(response.context["tasks"][1], task1)
-
 
     def test_index_get_order_due(self):
         task1 = Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
